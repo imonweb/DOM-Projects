77af385689ecf7b1421ad6b833f209b1b354fd1b
Imon <contact@imonweb.co.uk>
2023-05-07 00:43:48 +0100

    completed

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -24,10 +24,11 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', function(){
   const randomNumber = getRandomNumber;
   console.log(randomNumber);
+  
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber(){
-  return Math.random();
+  return Math.random()*colors.length;
 }
