017c86e430db35fdcb67caf84950e93011f71a5d
Imon <contact@imonweb.co.uk>
2023-05-23 09:23:52 +0100

    questions

--- questions/app.js
+++ questions/app.js
@@ -1,5 +1,27 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+  // console.log(question);
+  const btn = question.querySelector('.question-btn');
+  // console.log(btn);
+
+  btn.addEventListener('click', () => {
+
+    questions.forEach(function(item){
+      if( item !== question ){
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle('show-text');
+  })
+})
+
+ 
+
 // traversing the dom
+/*
 const btns = document.querySelectorAll('.question-btn');
  
 const questionText = document.querySelector('.question-text');
@@ -13,5 +35,5 @@ btns.forEach(function(btn){
 
   })
 })
-
+*/
  
