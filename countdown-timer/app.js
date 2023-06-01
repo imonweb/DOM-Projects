3666afaf6fdc3d8cf0024dcd610d24c9bac2e0d9
Imon <contact@imonweb.co.uk>
2023-06-01 15:23:54 +0100

    countdown-timer

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -26,8 +26,15 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2023,5,18,17,30,0);
-console.log(futureDate);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 11, 11, 30, 0);
+
+// let futureDate = new Date(2023,5,18,17,30,0);
+// console.log(futureDate);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
